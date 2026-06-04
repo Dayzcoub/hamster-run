aece032f0b305f9d9bfa0d4204c55ef0d98c33b1
Dayzcoub <77221495+Dayzcoub@users.noreply.github.com>
2026-06-04 15:41:43 +0200

    Rollback custom slide renderer import

--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -1,7 +1,6 @@
 import '../render/CanvasRendererPolish.js';
 import '../render/CanvasRendererPerformance.js';
 import '../render/CanvasRendererCleanRoad.js';
-import '../render/CanvasRendererSlideAsset.js';
 import { CanvasRenderer } from '../render/CanvasRenderer.js';
 import { LevelController } from '../gameplay/LevelController.js';
 
