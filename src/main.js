752a332b4804d693e83b24d8a0d18ce6e7d92773
Dayzcoub <77221495+Dayzcoub@users.noreply.github.com>
2026-06-05 18:50:24 +0200

    Load spaniel rescue gameplay patch

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './hit-feedback-patch.js';
 import './final-countdown-patch.js';
 import './render/CanvasRendererSpanielCompanion.js';
+import './spaniel-rescue-patch.js';
 import { Game } from './core/Game.js';
 
 const app = document.querySelector('#app');
