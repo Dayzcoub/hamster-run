5cf00adc4a4223c6ff5d427f774d8d27012a7ef8
Dayzcoub <77221495+Dayzcoub@users.noreply.github.com>
2026-06-05 09:27:21 +0200

    Add second level collectible resources

--- src/data/objects.js
+++ src/data/objects.js
@@ -5,6 +5,12 @@ export const objectCatalog = {
   bolts: { type: 'collectible', visualKey: 'bolt', resource: 'bolts', value: 1 },
   deck: { type: 'collectible', visualKey: 'stage_deck', resource: 'deck', value: 1 },
 
+  // Temporary visual aliases until dedicated collectible sprites are added.
+  powercon: { type: 'collectible', visualKey: 'cable_coil', resource: 'powercon', value: 1 },
+  tape: { type: 'collectible', visualKey: 'bolt', resource: 'tape', value: 1 },
+  led: { type: 'collectible', visualKey: 'stage_deck', resource: 'led', value: 1 },
+  truss: { type: 'collectible', visualKey: 'c2_connector', resource: 'truss', value: 1 },
+
   case: { type: 'obstacle', visualKey: 'flight_case', dodge: 'jump', damage: 1, clearanceHeight: 30 },
   cable_loop: { type: 'obstacle', visualKey: 'cable_loop', dodge: 'jump', damage: 1, clearanceHeight: 18 },
   mic_stand: { type: 'obstacle', visualKey: 'mic_stand', dodge: 'slide', damage: 1 },
