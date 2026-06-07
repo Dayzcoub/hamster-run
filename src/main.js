cecb19207eea14f3571498b52926b9fa0840eefe
Dayzcoub <77221495+Dayzcoub@users.noreply.github.com>
2026-06-07 20:38:29 +0200

    Use in-screen truss debug controls only

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import './hit-feedback-patch.js';
 import './final-countdown-patch.js';
 import './gameplay-visual-tuning.js';
-import './truss-debug-panel.js';
 import './render/CanvasRendererSpanielCompanion.js';
 import './spaniel-rescue-patch.js';
 import { Game } from './core/Game.js';
