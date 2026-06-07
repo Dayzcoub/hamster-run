87c4393ad8f011dde1d6732dcd912394442f5dbe
Dayzcoub <77221495+Dayzcoub@users.noreply.github.com>
2026-06-07 22:32:54 +0200

    Disable debug panel imports to restore game boot

--- src/main.js
+++ src/main.js
@@ -5,8 +5,6 @@ import './render/CanvasRendererSpanielCompanion.js';
 import './spaniel-rescue-patch.js';
 import { Game } from './core/Game.js';
 import './render/CanvasRendererPerspectiveAlignmentV1.js';
-import './truss-debug-panel.js';
-import './spawn-assets-debug-panel.js';
 
 const app = document.querySelector('#app');
 const game = new Game(app);
