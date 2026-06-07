d51fe03910a449d92860c4479d501683c92d74da
Dayzcoub <77221495+Dayzcoub@users.noreply.github.com>
2026-06-07 07:28:51 +0200

    Use production visual tuning config

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './hit-feedback-patch.js';
 import './final-countdown-patch.js';
-import './gameplay-perspective-tuning.js';
+import './gameplay-visual-tuning.js';
 import './render/CanvasRendererSpanielCompanion.js';
 import './spaniel-rescue-patch.js';
 import { Game } from './core/Game.js';
