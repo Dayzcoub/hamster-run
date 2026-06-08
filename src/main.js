a000caceb40d31b729bb014d4b6d2ba83f7be47d
Dayzcoub <77221495+Dayzcoub@users.noreply.github.com>
2026-06-08 07:56:58 +0200

    Connect debug mode safety reset

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './hit-feedback-patch.js';
 import './final-countdown-patch.js';
 import './gameplay-visual-tuning.js';
+import './debug-mode-safety.js';
 import './render/CanvasRendererSpanielCompanion.js';
 import './spaniel-rescue-patch.js';
 import { Game } from './core/Game.js';
